f7bcfbde837cd0d5a329ac8d3a51d375ebdbab85
Archita Nemalikanti <apn32@cornell.edu>
2026-05-21 19:59:15 -0400

    Initial Commit

---
+++ CSEvents/CSEventsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSEventsApp.swift
+//  CSEvents
+//
+//  Created by Archita Nemalikanti on 5/21/26.
+//
+
+import SwiftUI
+
+@main
+struct CSEventsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
